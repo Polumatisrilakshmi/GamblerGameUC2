65b48c4451af97924f117120237a727e1f6aaab3
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-04-20 10:56:01 +0530

    Added: Bet Per Day For A Gambler

---
+++ GamblerStartGameUC1.java
@@ -0,0 +1,14 @@
+package com.bridgelabzs;
+import java.util.Scanner;
+
+public class GamblerStartGameUC1 {
+
+    public static void main(String[] args) {
+
+       
+            int Stake = 100;
+            int Bet = 1;
+            System.out.println("the stakeholder should bet per day: " + Stake+"$");
+            System.out.println("the bet for one game :" + Bet+"$");
+        }
+    }
